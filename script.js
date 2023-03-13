7febe884e968210be1ca1e68002470cdd68b02b3
khantuevao <khantuevao@gmail.com>
2023-03-13 19:55:38 +0600

    Create a function that plays a round & returns a winner

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ function getComputerChoice() {
 
 //play a round, decide the winner and return a string with results
 function playRound(playerSelection, computerSelection) {
-    let winner;
-
-
-
-    
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection;
+    if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper')) {
+        return `You win! ${player} beats ${computer}`
+    } else if ((player === 'rock' && computer === 'paper') || (player === 'paper' && computer === 'scissors') || (player === ' scissors' && computer === 'rock')) {
+        return `You lose! ${computer} beats ${player}`
+    } else {
+        return 'It\'s a draw'
+    }
 }
